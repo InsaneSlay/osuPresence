4d1c73b28b1699e5494195af775bc3824e8ed9e5
Preston <46385879+InsaneSlay@users.noreply.github.com>
2021-09-19 19:36:49 -0400

    cleaned up and fixed bugs
    fixed bugs that caused status to display "+" for mods and added some new features

--- osu!Presence/Program.cs
+++ osu!Presence/Program.cs
@@ -3,6 +3,7 @@ using DiscordRPC.Logging;
 using OppaiSharp;
 using OsuMemoryDataProvider;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -10,6 +11,7 @@ using System.Threading;
 using System.Threading.Tasks;
 
 using static OppaiSharp.Helpers;
+using static osu_Presence.Win.osu_;
 namespace osu_Presence
 {
     class Program
@@ -17,6 +19,12 @@ namespace osu_Presence
         public static DiscordRpcClient client;
         public static OsuMemoryReader memoryReader;
         public static PPv2 ppCount;
+
+
+        public static ulong cTime;
+        public static ulong nTime;
+        public static string username;
+
         static async Task Main(string[] args)
         {
             Console.Title = "osu!Presence";
@@ -25,6 +33,7 @@ namespace osu_Presence
             client = new DiscordRpcClient("887247757046325248");
             client.Logger = new ConsoleLogger() { Level = LogLevel.Warning };
             client.Initialize();
+            username = File.ReadAllLines($"{GetOsuRunningDirectory()}//osu!.{Environment.UserName}.cfg")[0].Split(' ')[4];
             UpdatePresence();
             while (true)
             {
@@ -40,17 +49,18 @@ namespace osu_Presence
 
             byte[] data = new WebClient().DownloadData("https://osu.ppy.sh/osu/" + memoryReader.GetMapId());
             var stream = new MemoryStream(data, false);
-            var reader = new StreamReader(stream);
+            var reader = new StreamReader(new MemoryStream(data, false));
             var beatmap = Beatmap.Read(reader);
             Mods mods = (Mods)memoryReader.GetMods();
-            string Smods = "+" + ModsToString(mods);
+            string Smods = "";
+            if (!string.IsNullOrEmpty(ModsToString(mods)))
+                Smods = "+" + ModsToString(mods);
             var diff = new DiffCalc().Calc(beatmap, mods);
             switch (status)
             {
                 case 2:                
                     if (!memoryReader.IsReplay())
                     {
-                        ulong cTime, nTime;
                         if (Smods.Contains("DT") || Smods.Contains("NC"))
                         {
                             cTime = Convert.ToUInt64(DateTimeOffset.Now.ToUnixTimeMilliseconds());
@@ -66,120 +76,94 @@ namespace osu_Presence
 
                         ppCount = new PPv2(new PPv2Parameters(beatmap, diff, c300: memoryReader.ReadHit300(), c100: memoryReader.ReadHit100(), c50: memoryReader.ReadHit50(), cMiss: memoryReader.ReadHitMiss(), mods: mods));
                         if (double.IsNaN(ppCount.ComputedAccuracy.Value()))
-                            UpdateStatusPlaying($"{Smods} [{diff.Total:F2}*] {beatmap.Title} [{beatmap.Version}]", 
+                            UpdateStatus($"{Smods} [{diff.Total:F2}*] {beatmap.Title} [{beatmap.Version}]", 
                                 $"{memoryReader.ReadCombo()}x | 0.00% | 0pp", 
                                 getGameMode(memoryReader.ReadSongSelectGameMode()),
-                                cTime,
-                                nTime);
+                                true,
+                                true,
+                                true);
                         else if (double.IsNaN(ppCount.Total))
                         {                            
                             ppCount = new PPv2(new PPv2Parameters(beatmap, diff, c100: 0, combo: memoryReader.ReadCombo(), mods: mods));
-                            UpdateStatusPlaying($"{Smods} [{diff.Total:F2}*] {beatmap.Title} [{beatmap.Version}]"
+                            UpdateStatus($"{Smods} [{diff.Total:F2}*] {beatmap.Title} [{beatmap.Version}]"
                                 , $"SS | {memoryReader.ReadCombo()}x | {ppCount.ComputedAccuracy.Value() * 100:F2}% | {ppCount.Total:F2}pp",
                                 getGameMode(memoryReader.ReadSongSelectGameMode()),
-                                cTime,
-                                nTime);
+                                true,
+                                true,
+                                true);
                         }
                         else
-                            UpdateStatusPlaying($"{Smods} [{diff.Total:F2}*] {beatmap.Title} [{beatmap.Version}]",
+                            UpdateStatus($"{Smods} [{diff.Total:F2}*] {beatmap.Title} [{beatmap.Version}]",
                                 $"{memoryReader.ReadCombo()}x | {ppCount.ComputedAccuracy.Value() * 100:F2}% | {ppCount.Total:F2}pp",
                                 getGameMode(memoryReader.ReadSongSelectGameMode()),
-                                cTime,
-                                nTime);
+                                true,
+                                true,
+                                true);
                     }
                     else
-                        UpdateStatusButton($"Watching A Replay By {memoryReader.PlayerName()}", $"[{diff.Total:F2}*] {beatmap.Title} [{beatmap.Version}]", getGameMode(memoryReader.ReadSongSelectGameMode()));
+                        UpdateStatus($"Watching A Replay By {memoryReader.PlayerName()}", $"[{diff.Total:F2}*] {beatmap.Title} [{beatmap.Version}]", getGameMode(memoryReader.ReadSongSelectGameMode()), false, true, true);
                     break;
                 case 0:
-                    UpdateStatus("Main Menu", "Idle", getGameMode(memoryReader.ReadSongSelectGameMode()));
-                  break;
+                    UpdateStatus("Main Menu", "Idle", getGameMode(memoryReader.ReadSongSelectGameMode()), false, false,true);
+                    break;
                 case 5:
-                    UpdateStatus("Playing Solo", "Selecting A Song", getGameMode(memoryReader.ReadSongSelectGameMode()));
+                    UpdateStatus("Playing Solo", "Selecting A Song", getGameMode(memoryReader.ReadSongSelectGameMode()), false, false, true);
                     break;
                 case 15:
-                    UpdateStatus("osu!Direct", "Downloading Songs", getGameMode(memoryReader.ReadSongSelectGameMode()));
+                    UpdateStatus("osu!Direct", "Downloading Songs", getGameMode(memoryReader.ReadSongSelectGameMode()), false, false, true);
                     break;
                 case 4:
-                    UpdateStatus("Editor", "Editing A Map", getGameMode(memoryReader.ReadSongSelectGameMode()));
+                    UpdateStatus("Editor", "Editing A Map", getGameMode(memoryReader.ReadSongSelectGameMode()), false, false, true);
                     break;
                 case 11:
-                    UpdateStatus("Playing Multiplayer", "Searching for a lobby", getGameMode(memoryReader.ReadSongSelectGameMode()));
+                    UpdateStatus("Playing Multiplayer", "Searching for a lobby", getGameMode(memoryReader.ReadSongSelectGameMode()), false, false, true);
                     break;
                 case 7:
-
                     try
                     {
                         if (!double.IsNaN(ppCount.Total))
-                            UpdateStatusButton($"Finished: [{diff.Total:F2}*] {beatmap.Title} [{beatmap.Version}]",$" {ppCount.ComputedAccuracy.Value() * 100:F2}% | {ppCount.Total:F2}pp", getGameMode(memoryReader.ReadSongSelectGameMode()));
+                            UpdateStatus($"Finished: [{diff.Total:F2}*] {beatmap.Title} [{beatmap.Version}]",$" {ppCount.ComputedAccuracy.Value() * 100:F2}% | {ppCount.Total:F2}pp", getGameMode(memoryReader.ReadSongSelectGameMode()), false, true, true);
                         else
-                            UpdateStatusButton($"Finished Watching A Replay", $"[{diff.Total:F2}*] {beatmap.Title} [{beatmap.Version}]", getGameMode(memoryReader.ReadSongSelectGameMode()));
+                            UpdateStatus($"Finished Watching A Replay", $"[{diff.Total:F2}*] {beatmap.Title} [{beatmap.Version}]", getGameMode(memoryReader.ReadSongSelectGameMode()), false, true, true);
                     }
-                    catch { UpdateStatusButton($"Finished Watching A Replay", $"[{diff.Total:F2}*] {beatmap.Title} [{beatmap.Version}]", getGameMode(memoryReader.ReadSongSelectGameMode())); }
+                    catch { UpdateStatus($"Finished Watching A Replay", $"[{diff.Total:F2}*] {beatmap.Title} [{beatmap.Version}]", getGameMode(memoryReader.ReadSongSelectGameMode()), false, true, true); }
                     break;
             }
-        }
+        }    
 
-        public static void UpdateStatusButton(string details, string state, string gamemode)
+        public static void UpdateStatus(string details, string state, string gamemode, bool hasTime, bool hasMapBtn, bool hasAccBtn)
         {
-            client.SetPresence(new RichPresence()
+
+            List<Button> buttons = new List<Button>();
+
+
+            RichPresence presence = new RichPresence();
+            presence.Details = details;
+            presence.State = state;
+            presence.Assets = new Assets()
             {
-                Details = details,
-                State = state,
-                Assets = new Assets()
-                {
-                    LargeImageKey = "osulogo",
-                    SmallImageText = gamemode,
-                    SmallImageKey = gamemode.Replace("!", ""),
-                    LargeImageText = $"{memoryReader.PlayerName()} ",
+                LargeImageKey = "osulogo",
+                SmallImageText = gamemode,
+                SmallImageKey = gamemode.Replace("!", ""),
+                LargeImageText = "osu!Presence v1.0",
 
-                },
-                Buttons = new Button[]
+            };
+            if (hasTime)
+                presence.Timestamps = new Timestamps()
                 {
-                    new Button() { Label = "Download Map", Url = "https://osu.ppy.sh/beatmapsets/" + memoryReader.GetMapSetId() }
-                }
-            });
-        }
+                    StartUnixMilliseconds = cTime,
+                    EndUnixMilliseconds = nTime
+                };
 
-        public static void UpdateStatusPlaying(string details, string state, string gamemode, ulong start, ulong end)
-        {
-            client.SetPresence(new RichPresence()
-            {
-                Details = details,
-                State = state,
-                Timestamps = new Timestamps()
-                {
-                    StartUnixMilliseconds = start,
-                    EndUnixMilliseconds = end
-                },
-                Assets = new Assets()
-                {
-                    LargeImageKey = "osulogo",
-                    SmallImageText = gamemode,
-                    SmallImageKey = gamemode.Replace("!", ""),
-                    LargeImageText = $"{memoryReader.PlayerName()} ",
+            if (hasAccBtn)
+                buttons.Add(new Button() { Label = "View Profile", Url = "https://osu.ppy.sh/u/" + username });
 
-                },
-                Buttons = new Button[]
-                {
-                    new Button() { Label = "Download Map", Url = "https://osu.ppy.sh/beatmapsets/" + memoryReader.GetMapSetId() }
-                }
-            });
-        }
-        public static void UpdateStatus(string details, string state, string gamemode)
-        {
-            client.SetPresence(new RichPresence()
-            {
-                Details = details,
-                State = state,
-                Assets = new Assets()
-                {
-                    LargeImageKey = "osulogo",
-                    SmallImageText = gamemode,
-                    SmallImageKey = gamemode.Replace("!", ""),
-                    LargeImageText = $"{memoryReader.PlayerName()} ",
+            if (hasMapBtn)
+                buttons.Add(new Button() { Label = "Download Map", Url = "https://osu.ppy.sh/beatmapsets/" + memoryReader.GetMapSetId() });
 
-                }
-            });
+            presence.Buttons = buttons.ToArray();
+
+            client.SetPresence(presence);
         }
         
         public static string getGameMode(int val)
---
+++ osu!Presence/Win/osu!.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osu_Presence.Win
+{
+    public static class osu_
+    {
+        public static string GetOsuRunningDirectory()
+        {
+            var processes = Process.GetProcessesByName("osu!");
+            if (processes.Length > 0)
+            {
+                string dir = processes[0].Modules[0].FileName;
+                dir = dir.Remove(dir.LastIndexOf('\\'));
+                return dir;
+            }
+            return string.Empty;
+        }
+    }
+}
